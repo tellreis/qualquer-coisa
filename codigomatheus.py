edf66bc96fb12c230b7e4a869881834c6041646c
tellreis <eliezerpivotto33@gmail.com>
2022-10-10 14:46:21 -0300

    projeto media

---
+++ codigomatheus.py
@@ -0,0 +1,8 @@
+n1 = int(input ("digite a nota1 : "))
+n2 = int(input ("digite a nota2 : "))
+n3 = int(input ("digite a nota3 : "))
+n4 = int(input ("digite a nota4 : ")) 
+soma = int(n1 + n2 + n3 + n4)
+média = (soma / 4)
+print("soma: ", soma, "média", média)
+
